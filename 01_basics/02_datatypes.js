a40dfc73e20af8b2e774ecc1446feebab1a7aa5a
RajatMishra0 <mishrarj5092495@gmail.com>
2024-06-08 22:12:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" ;//treat all js code as newer version 
+// alert(3+3) // we are using node not brower so it give error as output 
+console.log(3
+    +3
+) // bad practice code should be readable highly
+console.log("rajat")
+
+let name = "hitesh" // string data typr 
+let age = 18  // number data type 
+let isLoggedIn= false 
+let state = null 
+
+
+// types of datatypes
+// number => range fron 2 to power 53 tak iss bada chayiye tho use 
+// bigint 
+//string =""
+// null => standalone value 
+// undefined => value not assigned 
+// symbol => unique 
+
+
+
+// object 
+console.log(typeof name)// output -> string datatype
+console.log(typeof age) // output -> number 
+console.log(typeof null)// output -> object datatype(it is error/khami of language) 
+console.log(typeof undefined) // output -> undefined datatypes
+
+
