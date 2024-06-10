448ac6deb071825c743ef9be9f297700f0b29be2
RajatMishra0 <mishrarj5092495@gmail.com>
2024-06-10 07:10:24 +0000

    conersion od datatypes

---
+++ 01_basics/03_connversionoperation.js
@@ -0,0 +1,33 @@
+let score =undefined
+//console.log(typeof score)
+//console.log(score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+
+
+// 33=> 33
+// 33abc => NaN
+// true => 1; false=> 0 ;
+// null => 0 ;
+// undefiend => NaN
+
+let isLoggedIn = 1 ;
+let isBoolean = Boolean(isLoggedIn)
+console.log(typeof isBoolean)
+console.log(isBoolean)
+
+// 1=> true ; 0=> false 
+// ""(empty string)=> false 
+// "rajat"=> true 
+
+
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
