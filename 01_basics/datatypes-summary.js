a30e53070f8caf36e6ee69a4a014b932e8a93079
RajatMishra0 <mishrarj5092495@gmail.com>
2024-07-09 20:43:07 +0000

    done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,16 @@
+//# primitive 
+
+// 7 types : string  , number , boolean , null , undefined , symbol , BigInt
+
+const score = 110
+const scoreValue = 100.3
+const isLoggedIn = false 
+const outside  = null 
+let userEmail ;
+
+const id =Symbol('123')
+const anotherid = Symbol('123') 
+console.log(id == anotherid) ;
+
+// reference(non primitive)
+// array  ,objects , functions 
