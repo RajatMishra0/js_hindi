60f4a4ba754cceb01b12c0d3f68c82825afe2e19
RajatMishra0 <mishrarj5092495@gmail.com>
2024-07-09 20:44:17 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//  console.log("2">1);
+//  console.log("02">1) ;
+
+
+// console.log(null>0) // false
+// console.log(null == 0);// false
+// console.log(null>= 0) ;// true 
+// the answer we get is due the comaparison convert the null in to number 0  so compiler treat it as zero 
+
+//*************video 9 primitive and non primitive */
