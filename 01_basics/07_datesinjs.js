56a996c5a24cff9325049c4614a3f8037c134bda
RajatMishra0 <mishrarj5092495@gmail.com>
2024-07-10 20:24:52 +0000

    done

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,47 @@
+
+// let myDate = new Date()
+// console.log(myDate) ;
+
+
+//  console.log(myDate.toString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+// console.log(myDate.toJSON());
+// console.log(myDate.getTimezoneOffset ());
+
+// // let myCreatedDate = new Date(2023 , 0 , 23)
+// let myCreatedDate = new Date(2023 , 0 , 23 , 5,30, 12) 
+// let myCreatedDate = new Date("2023-01-14")
+//console.log(myCreatedDate.toLocaleString()) ;
+
+
+//+++++++++++++++ time stamp +++++++++
+// used in quizzes , poles desiging 
+
+
+let myCreatedDate = new Date("2023-01-14")
+
+let myTimeStamp = Date.now()
+
+ //console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+//console.log(Date.now()) ;
+
+
+ let newDate = new Date() 
+//  console.log(newDate) ;
+//  console.log(newDate.getMonth()+1);
+//   console.log(newDate.getDay());
+
+
+
+
+
+
+
+
+
+
+
