c1c8a588cde82714f28cf0270299c6692b8e71cf
RajatMishra0 <mishrarj5092495@gmail.com>
2024-07-11 17:10:17 +0000

    done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+const myArr= [0, 1,2,3,4,5] ;
+const myHeros = ["shaktimaan", "naagraj"]
+// console.log(myHeros) ;
+
+// const myArr2 = new Array(1,2,3,4)
+// myArr.push(6)
+// myArr.pop()
+//myArr.unshift(9) ; //insert the arrr in front of the of array
+
+// myArr.shift() ;
+// myArr.shift() ;
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3)) ;
+
+// const newArr = myArr.join() // it change array in 
+// // the string format 
+// console.log(myArr);
+// console.log(newArr) ;
+
+
+// slice , splice
+// # interview question
+// difference between slice and splice Array in javascript ;
+
+console.log("A" ,myArr);
+const myn1 = myArr.slice(1,3)
+
+ console.log(myn1) ;
+ console.log("B" ,myArr) ;
+
+ const myn2 = myArr.splice(1,3)
+ console.log("C" ,myArr);
+ console.log(myn2);
+ 
+
