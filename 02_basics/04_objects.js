2663be359e1c6462759701c17ee0108905162087
RajatMishra0 <mishrarj5092495@gmail.com>
2024-07-12 09:43:13 +0000

    done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,5 +24,25 @@ const obj3 ={5:"a" ,6:"b"}
 
 // 2nd method 
 const obj6={...obj1,...obj2,...obj3}
-console.log(obj6);
+// console.log(obj6);
+
+
+const users=[
+    {
+        id: 1 ,
+        email:"rajat@gmail.com"
+    },
+    {
+        id: 2 ,
+        email:"rajat1@gmail.com"
+    },
+    ]
+    // console.log(users[1].id);
+
+    console.log(Object.keys(tinderUser));
+    console.log(Object.values(tinderUser));
+    console.log(Object.entries(tinderUser));
+
+    console.log(tinderUser.hasOwnProperty('isLogged'));
+
 
