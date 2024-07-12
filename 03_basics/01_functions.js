68f1bb0b3ce83ee1ed2edf4355e27bdb4a03d4d6
RajatMishra0 <mishrarj5092495@gmail.com>
2024-07-12 17:06:49 +0000

    jai shree raam

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,4 +30,28 @@ function loginUserMessage(username){
     return`${username}just logged in`
 }
 //console.log(loginUserMessage("hitesh"));
-console.log(loginUserMessage()) ;
+
+
+function calculateCartPrice(val1,val2,...num1){
+  return num1 
+}
+console.log(calculateCartPrice(200 ,300,400));
+
+const user ={
+    username:"rajat",
+    price:199
+}
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}` );
+}
+// handleobject(user)
+// handleobject({
+//     username:"sam",
+//     price:399
+// })
+
+const myNewArray=[200 ,400,100,600] 
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray)) ;
