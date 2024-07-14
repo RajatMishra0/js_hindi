33296a8442c23b77bc378de056d24e2974e25e44
RajatMishra0 <mishrarj5092495@gmail.com>
2024-07-14 18:10:14 +0000

    jai shre ram

---
+++ 04_control_flow/one.js
@@ -0,0 +1,61 @@
+const isUserloggedIn = true 
+const temperature = 41 
+// if(isUserloggedIn){
+//  console.log("executed");
+// }
+
+// if(temperature<50){
+//     console.log("less than 50") ;
+// }
+// else{
+//     console.log("temperatur is greater than 50");
+// }
+// console.log(executed);
+
+// const score = 200 
+
+// if(score>100){
+//     let power ="fly"
+//     console.log(`user power:${power}`);
+
+// }
+// console.log(`user power: ${power`}) ;
+
+const balance = 1000 
+// if(balance>500) console.log("test");
+
+// if(balance >500) console.log("test"),console.log("test2") ; ye nhi krna hai
+
+// if (balance <500){
+//     console.log("less than 500") ;
+
+// }else if (balance<750){
+//     console.log("less tha 750");
+// }
+// else if(balance<900){
+//     console.log("less than 750");
+// }
+// else{
+//     console.log("less than 1200")
+// }
+
+
+
+
+const userloggedIn = true 
+const debitCard = true 
+const loggedInfromgoogle = false ;
+const loggedInfromEmail = true 
+
+
+
+// if (userloggedIn&& debitCard&& 2==3){
+//     console.log("allow to buy course") ;
+// }
+
+if(loggedInfromgoogle ||loggedInfromEmail){
+ console.log("user logged in");
+}
+
+
+// <,> ,<=,>=,==,!=,===,!==
